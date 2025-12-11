adbb45cf4b1211ca97395053ba428d77f61e021d
JoCarrasco <jogregcarrasco@gmail.com>
2025-12-11 19:14:41 -0400

    fix: keep on testing stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 #include <vector>
 
-int main(const int argc, const char * const argv[]) {
-    if (argc < 1) {
+#include "projects/projects.h"
+
+int main(const int argc, const char **argv) {
+    if (argc < 3) {
         std::cout << "Not enough arguments provided." << std::endl;
     }
 
-    const std::string firstCommand = argv[0];
+    const std::string firstCommand = argv[1];
+    const std::string secondCommand = argv[2];
 
     if (firstCommand.empty()) {
         std::cout << "Not enough arguments provided." << std::endl;
+    }
+
+    if (firstCommand == "projects") {
+        handle_projects_commands(argv);
     }
 
     return EXIT_SUCCESS;
